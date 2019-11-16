e46099346b67f0608418edc549f5bdf2f06031a8
Dheepak Krishnamurthy <me@kdheepak.com>
2019-11-16 11:46:58 -0700

    Update files

--- keyboards/planck/keymaps/kdheepak/keymap.c
+++ keyboards/planck/keymaps/kdheepak/keymap.c
@@ -30,77 +30,32 @@ enum planck_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
- */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC  , KC_Q     , KC_W    , KC_E    , KC_R  , KC_T     , KC_Y  , KC_U  , KC_I     , KC_O    , KC_P      , KC_BSPACE ,
     KC_TAB  , KC_A     , KC_S    , KC_D    , KC_F  , KC_G     , KC_H  , KC_J  , KC_K     , KC_L    , KC_SCOLON , KC_QUOTE  ,
-    KC_LSFT , KC_Z     , KC_X    , KC_C    , KC_V  , KC_B     , KC_N  , KC_M  , KC_COMMA , KC_DOT  , KC_SLASH  , KC_SFTENT ,
-    KC_HYPR , KC_LCTRL , KC_LALT , KC_LGUI , LOWER , KC_SPACE , KC_NO , RAISE , KC_LEFT  , KC_DOWN , KC_UP     , KC_RIGHT
+    KC_LSFT , KC_Z     , KC_X    , KC_C    , KC_V  , KC_B     , KC_N  , KC_M  , KC_COMMA , KC_DOT  , KC_UP     , KC_SFTENT ,
+    KC_HYPR , KC_LCTRL , KC_LALT , KC_LGUI , LOWER , KC_SPACE , KC_NO , RAISE , KC_SLASH , KC_LEFT , KC_DOWN   , KC_RIGHT
 ),
 
-/* Lower
- * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_DELETE , KC_1    , KC_2    , KC_3    , KC_4     , KC_5    , KC_6     , KC_7     , KC_8        , KC_9        , KC_0      , _______  ,
-    _______   , KC_EXLM , KC_AT   , KC_HASH , KC_DLR   , KC_PERC , KC_CIRC  , KC_AMPR  , KC_ASTR     , KC_LPRN     , KC_RPRN   , KC_GRAVE ,
-    _______   , KC_UNDO , KC_CUT  , KC_COPY , KC_PASTE , KC_FIND , KC_MINUS , KC_EQUAL , KC_LBRACKET , KC_RBRACKET , KC_BSLASH , _______  ,
-    _______   , _______ , _______ , _______ , _______  , _______ , KC_NO    , _______  , KC_LEFT     , KC_DOWN     , KC_UP     , KC_RIGHT
-),
+    KC_DELETE , KC_1    , KC_2    , KC_3    , KC_4     , KC_5    , KC_6     , KC_7     , KC_8        , KC_9              , KC_0               , _______         ,
+    _______   , _______ , _______ , _______ , _______  , _______ , KC_LEFT  , KC_DOWN  , KC_UP       , KC_RIGHT          , _______            , KC_GRAVE        ,
+    _______   , KC_UNDO , KC_CUT  , KC_COPY , KC_PASTE , KC_FIND , KC_MINUS , KC_EQUAL , KC_LBRACKET , KC_RBRACKET       , KC_BRIGHTNESS_UP   , _______         ,
+    _______   , _______ , _______ , _______ , _______  , _______ , KC_NO    , _______  , KC_BSLASH   , KC_AUDIO_VOL_DOWN , KC_BRIGHTNESS_DOWN , KC_AUDIO_VOL_UP
+)           ,
 
-/* Raise
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_DELETE , KC_F1   , KC_F2   , KC_F3   , KC_F4   , _______ , _______ , KC_KP_7 , KC_KP_8 , KC_KP_9   , KC_KP_SLASH    , _______     ,
-    _______   , KC_F5   , KC_F6   , KC_F7   , KC_F8   , _______ , _______ , KC_KP_4 , KC_KP_5 , KC_KP_6   , KC_KP_ASTERISK , _______     ,
-    _______   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ , _______ , KC_KP_1 , KC_KP_2 , KC_KP_3   , KC_KP_MINUS    , _______     ,
-    _______   , _______ , _______ , _______ , _______ , _______ , KC_NO   , _______ , KC_KP_0 , KC_KP_DOT , KC_KP_PLUS     , KC_KP_EQUAL
+    _______ , _______ , KC_UP   , _______  , _______ , _______ , _______ , KC_KP_7 , KC_KP_8 , KC_KP_9   , KC_KP_SLASH    , _______     ,
+    _______ , KC_LEFT , KC_DOWN , KC_RIGHT , _______ , _______ , _______ , KC_KP_4 , KC_KP_5 , KC_KP_6   , KC_KP_ASTERISK , _______     ,
+    _______ , _______ , _______ , _______  , _______ , _______ , _______ , KC_KP_1 , KC_KP_2 , KC_KP_3   , KC_KP_MINUS    , _______     ,
+    _______ , _______ , _______ , _______  , _______ , _______ , KC_NO   , _______ , KC_KP_0 , KC_KP_DOT , KC_KP_PLUS     , KC_KP_EQUAL
 ),
 
-/* Adjust (Lower + Raise)
- *                      v------------------------RGB CONTROL--------------------v
- * ,-----------------------------------------------------------------------------------
- * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|  Del |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
- */
 [_ADJUST] = LAYOUT_planck_grid(
-    KC_DELETE , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
-    _______   , _______ , AU_ON   , AU_OFF  , AU_TOG  , _______ , _______ , RGB_TOG , RGB_VAI , RGB_VAD , _______ , RESET   ,
-    _______   , _______ , MU_ON   , MU_OFF  , MU_TOG  , _______ , _______ , RGB_MOD , RGB_HUI , RGB_HUD , _______ , _______ ,
-    _______   , _______ , _______ , _______ , _______ , _______ , KC_NO   , _______ , _______ , _______ , _______ , _______
+    _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,
+    _______ , _______ , AU_ON   , AU_OFF  , AU_TOG  , _______ , _______ , RGB_TOG , RGB_VAI , RGB_VAD , _______ , RESET   ,
+    _______ , _______ , MU_ON   , MU_OFF  , MU_TOG  , _______ , _______ , RGB_MOD , RGB_HUI , RGB_HUD , _______ , _______ ,
+    _______ , _______ , _______ , _______ , _______ , _______ , KC_NO   , _______ , _______ , _______ , _______ , _______
 )
 
 };
@@ -110,51 +65,56 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
+#define BLANK_COLOR  { 0   , 0   , 0   }
+#define ALPHA_COLOR  { 255 , 255 , 255 }
+#define SYMBOL_COLOR  { 77 , 77 , 255 }
+#define ORTHO_COLOR  { 255 , 0   , 0   }
+#define OS_COLOR     { 255, 77 , 77   }
+#define LAYER_COLOR  { 0   , 0   , 255 }
+#define NUMPAD_COLOR { 255 , 255 , 0   }
+#define ARROW_COLOR  { 0   , 255 , 0   }
+#define SPACE_COLOR  { 255 , 255 , 255 }
+#define RESET_COLOR  { 255   , 255 , 255   }
+
 const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
     [0] = {
-        {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255},
-        {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255},
-        {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255},
-        {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255},        {130,255,255}        , {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}, {130,255,255}
+        ORTHO_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR  , ALPHA_COLOR  , ALPHA_COLOR  , ALPHA_COLOR  , ORTHO_COLOR  ,
+        ORTHO_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR  , ALPHA_COLOR  , ALPHA_COLOR  , SYMBOL_COLOR , SYMBOL_COLOR ,
+        ORTHO_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR , ALPHA_COLOR  , SYMBOL_COLOR , SYMBOL_COLOR , ARROW_COLOR  , ORTHO_COLOR  ,
+        LAYER_COLOR , ORTHO_COLOR , ORTHO_COLOR , ORTHO_COLOR , LAYER_COLOR ,        SPACE_COLOR        , LAYER_COLOR , SYMBOL_COLOR , ARROW_COLOR  , ARROW_COLOR  , ARROW_COLOR
     },
 
     [1] = {
-        { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255},
-        { 42,255,255}, {146,224,255}, {146,224,255}, {146,224,255}, {146,224,255}, {146,224,255}, {146,224,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255},
-        { 42,255,255}, {146,224,255}, {146,224,255}, {146,224,255}, {146,224,255}, {146,224,255}, {146,224,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255},
-        { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255}, { 42,255,255},        { 42,255,255}        , { 42,255,255}, { 32,255,234}, { 32,255,234}, { 32,255,234}, { 32,255,234}
+        ORTHO_COLOR , SYMBOL_COLOR , SYMBOL_COLOR , SYMBOL_COLOR , SYMBOL_COLOR , SYMBOL_COLOR , SYMBOL_COLOR , SYMBOL_COLOR , SYMBOL_COLOR , SYMBOL_COLOR , SYMBOL_COLOR , BLANK_COLOR  ,
+        BLANK_COLOR , BLANK_COLOR  , BLANK_COLOR  , BLANK_COLOR  , BLANK_COLOR  , BLANK_COLOR  , ARROW_COLOR  , ARROW_COLOR  , ARROW_COLOR  , ARROW_COLOR  , BLANK_COLOR  , SYMBOL_COLOR ,
+        BLANK_COLOR , OS_COLOR     , OS_COLOR     , OS_COLOR     , OS_COLOR     , OS_COLOR     , SYMBOL_COLOR , SYMBOL_COLOR , SYMBOL_COLOR , SYMBOL_COLOR , OS_COLOR     , BLANK_COLOR  ,
+        LAYER_COLOR , BLANK_COLOR  , BLANK_COLOR  , BLANK_COLOR  , LAYER_COLOR  ,         SPACE_COLOR         , LAYER_COLOR  , SYMBOL_COLOR , OS_COLOR     , OS_COLOR     , OS_COLOR
     },
 
     [2] = {
-        { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246},
-        { 89,255,246}, {169,120,255}, {169,120,255}, {169,120,255}, {169,120,255}, {169,120,255}, {169,120,255}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246},
-        { 89,255,246}, { 89,255,246}, {169,120,255}, {169,120,255}, {169,120,255}, {169,120,255}, {169,120,255}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246},
-        { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246},        { 89,255,246}        , { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}, { 89,255,246}
+        BLANK_COLOR , BLANK_COLOR , ARROW_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , NUMPAD_COLOR , NUMPAD_COLOR , NUMPAD_COLOR , NUMPAD_COLOR , BLANK_COLOR ,
+        BLANK_COLOR , ARROW_COLOR , ARROW_COLOR , ARROW_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , NUMPAD_COLOR , NUMPAD_COLOR , NUMPAD_COLOR , NUMPAD_COLOR , BLANK_COLOR ,
+        BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , NUMPAD_COLOR , NUMPAD_COLOR , NUMPAD_COLOR , NUMPAD_COLOR , BLANK_COLOR ,
+        LAYER_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , LAYER_COLOR ,        BLANK_COLOR        , LAYER_COLOR  , NUMPAD_COLOR , NUMPAD_COLOR , NUMPAD_COLOR , BLANK_COLOR
     },
 
     [3] = {
-        {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255},
-        {216,255,255}, {216,255,255}, {249,228,255}, {249,228,255}, {249,228,255}, {216,255,255}, {216,255,255}, {105,255,255}, {105,255,255}, {105,255,255}, {216,255,255}, {14,255,255},
-        {216,255,255}, {216,255,255}, {249,228,255}, {249,228,255}, {249,228,255}, {216,255,255}, {216,255,255}, {105,255,255}, {105,255,255}, {105,255,255}, {216,255,255}, {216,255,255},
-        {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255},        {216,255,255}        , {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255}, {216,255,255}
+        BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR ,
+        BLANK_COLOR , BLANK_COLOR , OS_COLOR    , OS_COLOR    , OS_COLOR    , BLANK_COLOR , BLANK_COLOR , OS_COLOR    , OS_COLOR    , OS_COLOR    , BLANK_COLOR , RESET_COLOR ,
+        BLANK_COLOR , BLANK_COLOR , OS_COLOR    , OS_COLOR    , OS_COLOR    , BLANK_COLOR , BLANK_COLOR , OS_COLOR    , OS_COLOR    , OS_COLOR    , BLANK_COLOR , BLANK_COLOR ,
+        LAYER_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , LAYER_COLOR ,        BLANK_COLOR        , LAYER_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR , BLANK_COLOR
     },
 
 };
 
 void set_layer_color(int layer) {
   for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-    HSV hsv = {
-      .h = pgm_read_byte(&ledmap[layer][i][0]),
-      .s = pgm_read_byte(&ledmap[layer][i][1]),
-      .v = pgm_read_byte(&ledmap[layer][i][2]),
+    RGB rgb = {
+      .r = ledmap[layer][i][0],
+      .g = ledmap[layer][i][1],
+      .b = ledmap[layer][i][2],
     };
-    if (!hsv.h && !hsv.s && !hsv.v) {
-        rgb_matrix_set_color( i, 0, 0, 0 );
-    } else {
-        RGB rgb = hsv_to_rgb( hsv );
-        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
-        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
-    }
+    rgb_matrix_set_color( i, rgb.r, rgb.g, rgb.b );
   }
 }
 
@@ -190,6 +150,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
   }
   return true;
+}
+
+void suspend_power_down_user(void)
+{
+    rgb_matrix_set_suspend_state(true);
+}
+
+void suspend_wakeup_init_user(void)
+{
+    rgb_matrix_set_suspend_state(false);
 }
 
 bool muse_mode = false;
@@ -258,3 +228,6 @@ Combo
 
 // const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
 // combo_t key_combos[COMBO_COUNT] = {COMBO(jk_combo, KC_ESC)};
+
+
+
