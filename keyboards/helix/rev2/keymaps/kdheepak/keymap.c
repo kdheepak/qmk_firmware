a75a5d9e6c22ed3916ddba5877e9c3e6ee106509
Dheepak Krishnamurthy <me@kdheepak.com>
2019-11-17 00:06:16 -0700

    Update keymap.c

--- keyboards/helix/rev2/keymaps/kdheepak/keymap.c
+++ keyboards/helix/rev2/keymaps/kdheepak/keymap.c
@@ -35,19 +35,17 @@ enum macro_keycodes {
   KC_SAMPLEMACRO,
 };
 
-#define SFT_ENT  SFT_T(KC_ENT)
-
 //Macros
 #define M_SAMPLE M(KC_SAMPLEMACRO)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT( \
-      KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_QUOTE, \
-      KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-      SFT_ENT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MINS, KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SFTENT , \
-      ADJUST,   KC_LCTL, KC_LALT, KC_HYPR, KC_LGUI, KC_SPC,  LOWER,   RAISE,   KC_SPC,  KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT \
+      KC_GRAVE,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_QUOTE, \
+      KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+      KC_ESC,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+      LSFT_T(KC_ESC),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MINS, KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SFTENT , \
+      KC_LCTL,          KC_LALT, ADJUST,  KC_HYPR, KC_LGUI, KC_SPC,  LOWER,   RAISE,   KC_SPC,  KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT \
       ),
 
   [_LOWER] = LAYOUT( \
