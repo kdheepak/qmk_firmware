b024348fdd56aecff321fd503899f54c1a9a6761
Dheepak Krishnamurthy <me@kdheepak.com>
2020-03-24 23:03:23 -0600

    Use GESC

--- keyboards/helix/rev2/keymaps/kdheepak/keymap.c
+++ keyboards/helix/rev2/keymaps/kdheepak/keymap.c
@@ -48,31 +48,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                        KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-      KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOTE, \
+      KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOTE, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSLS,  KC_SLSH,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SFTENT, \
       KC_LCTL, KC_LALT, ADJUST,  KC_HYPR, KC_LGUI, KC_SPC,  LOWER,    RAISE,    KC_SPC,  KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT \
       ),
 
   [_LOWER] = LAYOUT( \
-      KC_F1,        KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12, \
-      KC_EXLM,      KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,                   KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  _______, KC_DEL, \
-      S(KC_GRAVE),  _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
-      _______,      _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, KC_LBRC, KC_RBRC,  KC_BRIU, _______, \
-      _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLD,  KC_BRID, KC_VOLU \
+      KC_F1,        KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                           KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12, \
+      KC_EXLM,      KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,                         KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  _______, KC_DEL, \
+      S(KC_GRAVE),  _______, _______, _______, _______, _______,                         KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
+      _______,      _______, _______, _______, _______, _______, S(KC_BSLS), S(KC_SLSH), _______, _______, KC_LBRC, KC_RBRC,  KC_BRIU, _______, \
+      _______,      _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, KC_VOLD,  KC_BRID, KC_VOLU \
       ),
 
   [_RAISE] = LAYOUT( \
-      _______,  _______, _______, _______,  _______, _______,                         _______, _______,     _______, _______,   _______,        _______, \
-      _______,  _______, KC_UP,   _______,  _______, _______,                         _______, KC_KP_7,     KC_KP_8, KC_KP_9,   KC_KP_SLASH,    _______, \
-      KC_GRAVE, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,                         _______, KC_KP_4,     KC_KP_5, KC_KP_6,   KC_KP_ASTERISK, _______, \
-      _______,  _______, _______, _______,  _______, _______, S(KC_BSLS), S(KC_SLSH), _______, KC_KP_1,     KC_KP_2, KC_KP_3,   KC_KP_MINUS,    _______, \
-      _______,  _______, _______, _______,  _______, _______, _______,    _______,    _______, KC_KP_EQUAL, KC_KP_0, KC_KP_DOT, KC_KP_PLUS,     _______ \
+      _______,  _______, _______, _______,  _______, _______,                   _______, _______,     _______, _______,   _______,        _______, \
+      _______,  _______, KC_UP,   _______,  _______, _______,                   _______, KC_KP_7,     KC_KP_8, KC_KP_9,   KC_KP_SLASH,    _______, \
+      KC_GRAVE, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,                   _______, KC_KP_4,     KC_KP_5, KC_KP_6,   KC_KP_ASTERISK, _______, \
+      _______,  _______, _______, _______,  _______, _______, KC_LBRC, KC_RBRC, _______, KC_KP_1,     KC_KP_2, KC_KP_3,   KC_KP_MINUS,    _______, \
+      _______,  _______, _______, _______,  _______, _______, _______, _______, _______, KC_KP_EQUAL, KC_KP_0, KC_KP_DOT, KC_KP_PLUS,     _______ \
       ),
 
   [_ADJUST] =  LAYOUT( \
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                           KC_F7,   KC_F8,   KC_F9,      KC_F10,     KC_F11,  KC_F12, \
       _______, RESET,   _______,  _______, _______, _______,                        _______, _______, _______,    _______,    _______, _______, \
-      _______, _______, _______, _______, _______, _______,                         _______, _______, _______,    _______,    _______, _______, \
+      KC_ESC,  _______, _______, _______, _______, _______,                         _______, _______, _______,    _______,    _______, _______, \
       _______, _______, _______, _______, _______, _______, S(KC_LBRC), S(KC_RBRC), _______, _______, S(KC_LBRC), S(KC_RBRC), RGB_MOD, _______, \
       _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______,    _______,    RGB_TOG, _______ \
       )
