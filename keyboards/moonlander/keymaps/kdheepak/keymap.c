509292eb822dc26b9a88237f1ea6d8c665192ca7
Dheepak Krishnamurthy <me@kdheepak.com>
2020-09-16 15:06:19 -0600

    Update files

--- keyboards/moonlander/keymaps/kdheepak/keymap.c
+++ keyboards/moonlander/keymaps/kdheepak/keymap.c
@@ -19,35 +19,51 @@
 
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
-  HSV_0_255_255,
-  HSV_86_255_128,
-  HSV_172_255_255,
+  LOWER,
+  RAISE,
+  ADJUST,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,     _______,                   _______, KC_7,    KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,     KC_LBRC,                   KC_RBRC, KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,     KC_BSLS,                   KC_SLSH, KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOTE,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                                         KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_UP,   KC_SFTENT,
-    KC_LCTL, KC_LALT, KC_HYPR, MO(1),   KC_LGUI,            _______,                   _______,          KC_RALT,  MO(2),   KC_LEFT, KC_DOWN, KC_RIGHT,
-                                                  KC_SPC,   MO(1),   _______, _______, MO(2),   KC_SPC
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,   KC_BRID,                    KC_BRIU, KC_7,    KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,   KC_LBRC,                    KC_RBRC, KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,   KC_BSLS,                    KC_SLSH, KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOTE,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                                        KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_UP,   KC_SFTENT,
+    KC_LCTL, KC_LALT, KC_HYPR, MO(1),   KC_LGUI,          _______,                    _______,          KC_RALT,  MO(2),   KC_LEFT, KC_DOWN, KC_RIGHT,
+                                                  KC_SPC, TT(1),   TT(3),    TT(4),   TT(2),   KC_SPC
   ),
   [1] = LAYOUT_moonlander(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    _______,                   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_ACL0, _______,  KC_WH_L,                   KC_WH_R, _______, _______, _______, _______, _______, _______,
-    KC_GRV,  KC_MS_L, KC_MS_D, KC_MS_R, KC_ACL1, _______,  KC_WH_D,                   KC_WH_U, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-    _______, KC_BTN3, KC_BTN4, KC_BTN5, KC_ACL2, _______,                                      _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,           _______,                   _______,          _______, _______, _______, _______, _______,
-                                                 _______,   _______, _______, _______, _______, _______
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_VOLD,                    KC_VOLU, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_ACL0, _______, KC_WH_L,                    KC_WH_R, _______, _______, _______, _______, _______, _______,
+    KC_GRV,  KC_MS_L, KC_MS_D, KC_MS_R, KC_ACL1, _______, KC_WH_D,                    KC_WH_U, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+    _______, KC_BTN3, KC_BTN4, KC_BTN5, KC_ACL2, _______,                                      _______, _______, _______, _______, _______, KC_ENT,
+    _______, _______, _______, _______, _______,          _______,                    _______,          _______, _______, _______, _______, _______,
+                                                 _______, _______, _______,  _______, _______, _______
   ),
   [2] = LAYOUT_moonlander(
-    KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,    _______,                   _______, KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-    _______, KC_EXLM,  KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,  _______,                   _______, _______,  KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______,
-    RESET,   KC_HASH,  KC_DLR,  KC_LPRN, KC_RPRN, _______,  _______,                   _______, _______,  KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______,
-    _______, KC_PERC,  KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                                      _______,  KC_P1,   KC_P2,   KC_P3,   KC_PMNS, _______,
-    _______, KC_COMM,  _______, _______, _______,           _______,                   _______,           KC_PEQL, KC_P0,   KC_PDOT, KC_PPLS, _______,
-                                                 _______,   _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,                    _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______,
+    RESET,   KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, _______, _______,                    _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______,
+    _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,                                      _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, _______,
+    _______, KC_COMM, _______, _______, _______,          _______,                    _______,          KC_PEQL, KC_P0,   KC_PDOT, KC_PPLS, _______,
+                                                 _______, _______, _______,  _______, _______, _______
+  ),
+  [3] = LAYOUT_moonlander(
+    _______, _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______, _______,
+    _______, DM_REC1, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, DM_PLY1, _______,
+    _______, _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,                                      _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,          _______,                    _______,          _______, _______, _______, _______, _______,
+                                                 _______, _______, _______,  _______, _______, _______
+  ),
+  [4] = LAYOUT_moonlander(
+    _______, _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______, _______,
+    _______, DM_RSTP, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,                                      _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,          _______,                    _______,          _______, _______, _______, _______, _______,
+                                                 _______, _______, _______,  _______, _______, _______
   ),
 };
 
@@ -59,19 +75,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         rgblight_mode(1);
       }
       return false;
-    case HSV_0_255_255:
+    case LOWER:
       if (record->event.pressed) {
         rgblight_mode(1);
         rgblight_sethsv(0,255,255);
       }
       return false;
-    case HSV_86_255_128:
+    case RAISE:
       if (record->event.pressed) {
         rgblight_mode(1);
         rgblight_sethsv(86,255,128);
       }
       return false;
-    case HSV_172_255_255:
+    case ADJUST:
       if (record->event.pressed) {
         rgblight_mode(1);
         rgblight_sethsv(172,255,255);
